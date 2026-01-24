ff010a5a82d0b116e5b979b50c755678adbe6638
PainInVein <lesybinh2004@gmail.com>
2026-01-24 14:08:57 +0700

    Finnish Tag table

--- PRN232.NMS.API/Models/RequestModels/TagRequests/TagFilterRequest.cs
+++ PRN232.NMS.API/Models/RequestModels/TagRequests/TagFilterRequest.cs
@@ -4,11 +4,8 @@ namespace PRN232.NMS.API.Models.RequestModels.TagRequests
 {
     public class TagFilterRequest : PagedRequest
     {
-        [Required(ErrorMessage ="bye bye")]
         public string? SearchName { get; set; }
-        [Required(ErrorMessage = "bye byedsadas")]
         public string? SortOption { get; set; }
-        [Required(ErrorMessage = "bye bye")]
         public List<int>? NewArticleIds { get; set; }
     }
 }
