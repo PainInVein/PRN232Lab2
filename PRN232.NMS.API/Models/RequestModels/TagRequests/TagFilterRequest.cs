b493f36b7f317e22fd0d2ee168d8a32bc80f7a3d
PainInVein <lesybinh2004@gmail.com>
2026-01-24 13:56:27 +0700

    update auto validation

--- PRN232.NMS.API/Models/RequestModels/TagRequests/TagFilterRequest.cs
+++ PRN232.NMS.API/Models/RequestModels/TagRequests/TagFilterRequest.cs
@@ -1,9 +1,14 @@
-﻿namespace PRN232.NMS.API.Models.RequestModels.TagRequests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRN232.NMS.API.Models.RequestModels.TagRequests
 {
     public class TagFilterRequest : PagedRequest
     {
+        [Required(ErrorMessage ="bye bye")]
         public string? SearchName { get; set; }
+        [Required(ErrorMessage = "bye byedsadas")]
         public string? SortOption { get; set; }
+        [Required(ErrorMessage = "bye bye")]
         public List<int>? NewArticleIds { get; set; }
     }
 }
